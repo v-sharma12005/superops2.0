59af631bdba7e051dbfa05c5b35077ed3b49fd8f
Vishnu Sharma <vishnu.sharma.oist.com>
2026-02-15 21:04:34 +0530

    My Java Project

---
+++ area.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class area {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float rad = sc.nextFloat();
+       // float b = sc.nextFloat();
+        float area = 22/7*rad*rad;
+        System.out.println(area);
+
+
+    }
+    
+}
